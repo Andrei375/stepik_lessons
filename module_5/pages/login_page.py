de82d6608847388d52ac3249afe293db364463d6
Andrei Kharlap <akharlap80@gmail.com>
2021-03-07 21:06:06 +0300

    includes a transition to login page

--- module_5/pages/login_page.py
+++ module_5/pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):  # реализуйте проверку на корректный url адрес
-        assert self.browser.current_url.contains("login")
+        assert "login" in self.browser.current_url
 
     def should_be_login_form(self):  # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.INPUT_LOGIN_USERNAME), \
